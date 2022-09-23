7b798a59a5216112e52431990443841029dd2cc9
Nithush <93372131+van-der-NithusH-II@users.noreply.github.com>
2022-09-23 08:10:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  noStroke()
+  fill(random(colorlist));
+  ellipse(mouseX, mouseY, 25, 25);
+}
