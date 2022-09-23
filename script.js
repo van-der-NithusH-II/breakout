6068ff9e563ac4f3b0ada27f10dc27508b1e4028
Nithush <93372131+van-der-NithusH-II@users.noreply.github.com>
2022-09-23 08:49:57 +0000

    Blokjes gemaakt, contructor gemaakt en balletje gemaakt.

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
+class Block {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
 
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+  draw() {
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
+
+var blocks = []
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  createCanvas(600, 500);
+
+  
+  for(row = 0; row < 3; row++){
+    for(i = 0; i < 7; i++){
+      blocks.push(new Block((80 * i) + 10, (50 * row) + 10,70,30))
+    }  
+  }
+
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  background(225);
+
+  ellipse(310, 450, 20, 20)
+
+  fill("black");
+  
+  blocks.forEach(b=> {
+    b.draw();
+  })
+
+  
+
+
+}
+
+function keyPressed() {
+  if (keyCode == 37) {
+    block.x -= 5;
+  }
+  if (keyCode == 39) {
+    block.x += 5;
+  }
 }
